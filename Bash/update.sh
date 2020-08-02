29e0227106ec496f79341b6e0776342aa892ce2e
RichardWHZ <richardwhz@outlook.com>
2020-08-03 04:35:44 +0800

    Added xpaadneo and ventoy

--- Bash/update.sh
+++ Bash/update.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
-
-sudo apt update 
-sudo apt upgrade -y 
+# Update Packages
+sudo apt update
+sudo apt upgrade -y
 sudo apt autoremove -y
-sudo flatpak update
 
+# Update xpadneo
+cd ~/xpadneo
+git pull
+sudo ./update.sh
+
+# Update Ventoy
+cd ~/Ventoy
+git pull 
+cd ~
